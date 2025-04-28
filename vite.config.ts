a898f26e1b444212a207457b029a6d9605b0c163
Kevin Farrugia <hello@imkev.dev>
2025-04-28 14:06:03 +0200

    Updated manualChunks to place react in a separate chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            const parts = id.split("node_modules/");
+            if (parts.length > 1 && parts[1].includes("react")) {
+              return `react`;
+            }
+          }
+        },
+      },
+    },
+  },
+  plugins: [
+    react({
+      jsxRuntime: "classic",
+    }),
+  ],
+});
